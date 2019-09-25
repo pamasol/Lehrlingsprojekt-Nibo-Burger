b03b3053a57a93552f79ecacf8b241a7cf7f17b1
philippbruhin <info@philippbruhin.ch>
2019-09-25 17:41:52 +0200

    updated layout of main.c

--- PhilippBruhin/Excercise_tasks/a_flashing_led_1/a_flashing_led_1/main.c
+++ PhilippBruhin/Excercise_tasks/a_flashing_led_1/a_flashing_led_1/main.c
@@ -4,14 +4,14 @@
 void setup()
 {
 	/* Code that is executed once at startup */
-	  led_init();
+	led_init();
 }
 
 void loop()
 {
 	/* Code that is repeated infinitely on NIBO Burger */
-	  led_set(1, 1);
-	  delay(200);
-	  led_set(1, 0);
-	  delay(200);
+	led_set(1, 1);
+	delay(500);
+	led_set(1, 0);
+	delay(500);
 }
