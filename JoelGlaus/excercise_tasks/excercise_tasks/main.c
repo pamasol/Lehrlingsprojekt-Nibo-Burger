e0a245c6044c2a3358cf44da1baa389d1dbd345a
JoelGlaus <55971135+JoelGlaus@users.noreply.github.com>
2019-09-30 14:34:51 +0200

    Update main.c

--- JoelGlaus/excercise_tasks/excercise_tasks/main.c
+++ JoelGlaus/excercise_tasks/excercise_tasks/main.c
@@ -1,41 +1,41 @@
-/* Includes f¸r den Zugriff auf die NIBO Burger Bibliothek */
+/* Includes f√ºr den Zugriff auf die NIBO Burger Bibliothek */
 #include "niboburger/robomain.h"
 
 void setup()
 {
 	/* Programmcode zum einmaligen Setup des Roboters */
-	led_init();			// Anweisung um Led zu initialisieren
+	led_init();		// Anweisung um Led zu initialisieren
 	analog_init();		// Anweisung um Analog Signale zu initialisieren
 }
 
 void loop()
 {
-	/* Programmcode, den der NIBO Burger immer wieder ausf¸hren soll */
+	/* Programmcode, den der NIBO Burger immer wieder ausf√ºhren soll */
 	
 	char key = key_get_char();
 	
 	switch (key)
 	{
 		
-	case 'a' :			// Taster 1 nicht bet‰tigt
+	case 'a' :		// Taster 1 nicht bet√§tigt
 	led_set(1,0);		// Led 1 (rote Led) auf Zustand 0 setzen
-	break;				// Beendung der Switch Anweisung
-	case 'A' :			// Taster 1 bet‰tigt		
+	break;			// Beendung der Switch Anweisung
+	case 'A' :		// Taster 1 bet√§tigt		
 	led_set(1,1);		// Led 1 (rote Led) auf Zustand 1 setzen
-	break;				// Beendung der Switch Anweisung
+	break;			// Beendung der Switch Anweisung
 	
-	case 'b' :			// Taster 2 nicht bet‰tigt
+	case 'b' :		// Taster 2 nicht bet√§tigt
 	led_set(2,0);		// Led 2 (blaue Led) auf Zustand 0 setzen
-	break;				// Beendung der Switch Anweisung
-	case 'B' :			// Taster 2 bet‰tigt
+	break;			// Beendung der Switch Anweisung
+	case 'B' :		// Taster 2 bet√§tigt
 	led_set(2,1);		// Led 2 (blaue Led) auf Zustand 1 setzen
-	break;				// Beendung der Switch Anweisung
+	break;			// Beendung der Switch Anweisung
 	
-	case 'c' :			// Taster 3 nicht bet‰tigt
+	case 'c' :		// Taster 3 nicht bet√§tigt
 	led_set(3,0);		// Led 3 (blaue Led) auf Zustand 0 setzen
-	break;				// Beendung der Switch Anweisung
-	case 'C' :			// Taster 3 bet‰tigt
+	break;			// Beendung der Switch Anweisung
+	case 'C' :		// Taster 3 bet√§tigt
 	led_set(3,1);		// Led 3 (blaue Led) auf Zustand 1 setzen
-	break;				// Beendung der Switch Anweisung
+	break;			// Beendung der Switch Anweisung
 	}		
 }	
