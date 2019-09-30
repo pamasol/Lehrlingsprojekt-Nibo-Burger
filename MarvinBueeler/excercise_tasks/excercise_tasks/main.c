82c2154041053495cd36108127bb1c5f0af8e72e
marvinbueeler <marvin_00@outlook.com>
2019-09-30 11:09:33 +0200

    augfabe 6 gelöst

--- MarvinBueeler/excercise_tasks/excercise_tasks/main.c
+++ MarvinBueeler/excercise_tasks/excercise_tasks/main.c
@@ -15,15 +15,16 @@ void setup() //Setup wird einmal am Anfang ausgeführt
 
 void loop() //Loop wird ständig wiederholt
 {
-	int lednr;
+	int lednr, time; //neue Variable für die Zeit
 	
-	for (lednr=1; lednr<=4; lednr++)
+	for(time=50; time<400; time*=2) //Diese for-Schleife erhöht den Wert der Zeit
 	{
-		led_set(lednr,1);
-		delay(500);
-		led_set(lednr,0);
-		delay(500);
+		for (lednr=1; lednr<=4; lednr++) //for_schlaufe um die Leds anzusteuern
+		{
+			led_set(lednr,1);
+			delay(time); //time Variable wird für die Verzögerung genutzt
+			led_set(lednr,0);
+			delay(time);
+		}
 	}
 }
-
-//Mit der for-Schlaufe ist der Code viel Kürzer, da ich mit einem Command alle Leds ansteuern kann. Die Funtkion bleibt die gleiche.
