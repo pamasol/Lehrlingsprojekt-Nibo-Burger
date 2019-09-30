46db863472645019b749ca7ad25ed5d275c6ed6f
marvinbueeler <marvin_00@outlook.com>
2019-09-30 15:19:31 +0200

    aufgabe 9 gelöst

--- MarvinBueeler/excercise_tasks/excercise_tasks/main.c
+++ MarvinBueeler/excercise_tasks/excercise_tasks/main.c
@@ -12,23 +12,31 @@ void setup() //Setup wird einmal am Anfang ausgeführt
 {
 	led_init(); //Leds werden initialisiert
 	analog_init(); //alle analogen Datenleitungen (auch die für die Taster) werden initialisiert 
+	odometry_init(); //Odometry Sensoren werden initialisiert
 }
 
 void loop() //Loop wird ständig wiederholt
 {
+	int left = odometry_getLeft(0); //Variable für Wert von odometry Sensor links
+	int right = odometry_getRight(0); //Variable für Wert von odometry Sensor rechts
 	char key = key_get_char(); //char Variable key deklariert
 	
 	switch (key) {
 		case 'A':
-		led_set(1,1);
-		led_set(4,1);
-		break;
+			odometry_resetLeft();
+			led_set(1,1);
+			led_set(1,0);
+			break;
 		case 'B':
-		led_set(2,1);
-		led_set(3,1);
-		break;
+			odometry_resetRight();
+			break;
 		case 'C':
-		led_setall(0,0,0,0);
-		break;
+			odometry_reset();
+			break;
 	}
+	
+	led_set(1,left>10);
+	led_set(2,left>20);
+	led_set(3,right>20);
+	led_set(4,right>10);
 } 
