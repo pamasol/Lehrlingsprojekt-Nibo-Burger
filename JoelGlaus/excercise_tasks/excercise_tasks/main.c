9a4361dba3dcb7d8f89aae1c7e3812d5993a94a4
JoelGlaus <glaus.joel@gmail.com>
2019-09-30 14:26:03 +0200

    aufgabe 7 geloest

--- JoelGlaus/excercise_tasks/excercise_tasks/main.c
+++ JoelGlaus/excercise_tasks/excercise_tasks/main.c
@@ -4,24 +4,38 @@
 void setup()
 {
 	/* Programmcode zum einmaligen Setup des Roboters */
-	led_init();								// Anweisung um Led zu initialisieren
+	led_init();			// Anweisung um Led zu initialisieren
+	analog_init();		// Anweisung um Analog Signale zu initialisieren
 }
 
 void loop()
 {
 	/* Programmcode, den der NIBO Burger immer wieder ausführen soll */
-	int LedNr;
-	int time;
 	
-	for (time=50; time<=200; time*=2)		// führt Anweisungen mehrmals durch 
+	char key = key_get_char();
+	
+	switch (key)
 	{
 		
-		for (LedNr=1; LedNr<=4; LedNr++)	// führt Anweisungen mehrmals durch
-		{
-		led_set(LedNr,1);					// Led auf Zustand 1 setzen
-		delay(time);						// Zeitverzögerung um 500ms
-		led_set(LedNr,0);					// Led auf Zustand 0 setzen
-		delay(time);						// Zeitverzögerung um 500ms
-		}
-	}
-}
+	case 'a' :			// Taster 1 nicht betätigt
+	led_set(1,0);		// Led 1 (rote Led) auf Zustand 0 setzen
+	break;				// Beendung der Switch Anweisung
+	case 'A' :			// Taster 1 betätigt		
+	led_set(1,1);		// Led 1 (rote Led) auf Zustand 1 setzen
+	break;				// Beendung der Switch Anweisung
+	
+	case 'b' :			// Taster 2 nicht betätigt
+	led_set(2,0);		// Led 2 (blaue Led) auf Zustand 0 setzen
+	break;				// Beendung der Switch Anweisung
+	case 'B' :			// Taster 2 betätigt
+	led_set(2,1);		// Led 2 (blaue Led) auf Zustand 1 setzen
+	break;				// Beendung der Switch Anweisung
+	
+	case 'c' :			// Taster 3 nicht betätigt
+	led_set(3,0);		// Led 3 (blaue Led) auf Zustand 0 setzen
+	break;				// Beendung der Switch Anweisung
+	case 'C' :			// Taster 3 betätigt
+	led_set(3,1);		// Led 3 (blaue Led) auf Zustand 1 setzen
+	break;				// Beendung der Switch Anweisung
+	}		
+}	
