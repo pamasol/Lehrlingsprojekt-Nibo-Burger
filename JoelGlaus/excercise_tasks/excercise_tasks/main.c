7a7f55a768550540c1376b9d3b45e6ad36770bbf
JoelGlaus <glaus.joel@gmail.com>
2019-09-30 14:45:37 +0200

    aufgabe 8 geloest

--- JoelGlaus/excercise_tasks/excercise_tasks/main.c
+++ JoelGlaus/excercise_tasks/excercise_tasks/main.c
@@ -19,23 +19,20 @@ void loop()
 		
 	case 'a' :			// Taster 1 nicht betätigt
 	led_set(1,0);		// Led 1 (rote Led) auf Zustand 0 setzen
+	led_set(4,0);		// Led 4 (rote Led) auf Zustand 0 setzen
 	break;				// Beendung der Switch Anweisung
 	case 'A' :			// Taster 1 betätigt		
 	led_set(1,1);		// Led 1 (rote Led) auf Zustand 1 setzen
+	led_set(4,1);		// Led 4 (rote Led) auf Zustand 1 setzen
 	break;				// Beendung der Switch Anweisung
 	
 	case 'b' :			// Taster 2 nicht betätigt
 	led_set(2,0);		// Led 2 (blaue Led) auf Zustand 0 setzen
+	led_set(3,0);		// Led 3 (blaue Led) auf Zustand 0 setzen
 	break;				// Beendung der Switch Anweisung
 	case 'B' :			// Taster 2 betätigt
 	led_set(2,1);		// Led 2 (blaue Led) auf Zustand 1 setzen
-	break;				// Beendung der Switch Anweisung
-	
-	case 'c' :			// Taster 3 nicht betätigt
-	led_set(3,0);		// Led 3 (blaue Led) auf Zustand 0 setzen
-	break;				// Beendung der Switch Anweisung
-	case 'C' :			// Taster 3 betätigt
-	led_set(3,1);		// Led 3 (blaue Led) auf Zustand 1 setzen
+	led_set(3,1);		// Led 2 (blaue Led) auf Zustand 1 setzen
 	break;				// Beendung der Switch Anweisung
 	}		
 }	
