c5047791f22c8b68d90d79ea1b3e33a68c6151ad
JoelGlaus <55971135+JoelGlaus@users.noreply.github.com>
2019-09-30 16:35:31 +0200

    Update main.c

--- JoelGlaus/excercise_tasks/excercise_tasks/main.c
+++ JoelGlaus/excercise_tasks/excercise_tasks/main.c
@@ -1,36 +1,36 @@
-/* Includes für den Zugriff auf die NIBO Burger Bibliothek */
+/* Includes fÃ¼r den Zugriff auf die NIBO Burger Bibliothek */
 #include "niboburger/robomain.h"
 
 void setup()
 {
 	/* Programmcode zum einmaligen Setup des Roboters */
-	led_init();						// Anweisung um Led zu initialisieren
+	led_init();					// Anweisung um Led zu initialisieren
 	analog_init();					// Anweisung um Analog Signale zu initialisieren
 	odometry_init();				// Anweisung um Odometriesensoren zu initialisieren
 }
 
 void loop()
 {
-	/* Programmcode, den der NIBO Burger immer wieder ausführen soll */
+	/* Programmcode, den der NIBO Burger immer wieder ausfÃ¼hren soll */
 	char key = key_get_char();
 	
-	if (key == 'a')					// Wenn Taster 1 gedrückt wurde					
+	if (key == 'a')					// Wenn Taster 1 gedrÃ¼ckt wurde					
 	{
-	odometry_resetLeft();			// Rücksetzen des Wertes vom linken Odometriesensor
+	odometry_resetLeft();				// RÃ¼cksetzen des Wertes vom linken Odometriesensor
 	}
 
-	if (key == 'b')					// Wenn Taster 2 gedrückt wurde
+	if (key == 'b')					// Wenn Taster 2 gedrÃ¼ckt wurde
 	{
-	odometry_resetRight();			// Rücksetzen des Wertes vom rechten Odometriesensor
+	odometry_resetRight();				// RÃ¼cksetzen des Wertes vom rechten Odometriesensor
 	}
 
-	if (key == 'c')					// Wenn Taster 3 gedrückt wurde
+	if (key == 'c')					// Wenn Taster 3 gedrÃ¼ckt wurde
 	{
-	odometry_reset();				// Rücksetzen des Wertes beider Odometriesensoren
+	odometry_reset();				// RÃ¼cksetzen des Wertes beider Odometriesensoren
 	}
 
-	led_set(1, odometry_getLeft(0)>10);			// Led 1 (rote Led) auf Zustand 1 setzen, wenn der Wert des linken Odometriesensors 10 Umdrehungen gemacht hat
-	led_set(2, odometry_getLeft(0)>20);			// Led 2 (blaue Led) auf Zustand 1 setzen, wenn der Wert des linken Odometriesensors 20 Umdrehungen gemacht hat
+	led_set(1, odometry_getLeft(0)>10);		// Led 1 (rote Led) auf Zustand 1 setzen, wenn der Wert des linken Odometriesensors 10 Umdrehungen gemacht hat
+	led_set(2, odometry_getLeft(0)>20);		// Led 2 (blaue Led) auf Zustand 1 setzen, wenn der Wert des linken Odometriesensors 20 Umdrehungen gemacht hat
 
 	led_set(3, odometry_getRight(0)>10);		// Led 3 (blaue Led) auf Zustand 1 setzen, wenn der Wert des rechten Odometriesensors 10 Umdrehungen gemacht hat
 	led_set(4, odometry_getRight(0)>20);		// Led 4 (rote Led) auf Zustand 1 setzen, wenn der Wert des rechten Odometriesensors 20 Umdrehungen gemacht hat
