62f8c18265c1b88e29179826ff1411d624479527
marvinbueeler <marvin_00@outlook.com>
2019-09-30 12:06:25 +0200

    aufgabe 7 gelöst

--- MarvinBueeler/excercise_tasks/excercise_tasks/main.c
+++ MarvinBueeler/excercise_tasks/excercise_tasks/main.c
@@ -11,20 +11,33 @@
 void setup() //Setup wird einmal am Anfang ausgeführt
 {
 	led_init(); //Leds werden initialisiert
+	analog_init(); //alle analogen Datenleitungen (auch die für die Taster) werden initialisiert 
 }
 
 void loop() //Loop wird ständig wiederholt
 {
-	int lednr, time; //neue Variable für die Zeit
+	char key = key_get_char(); //char Variable key deklariert
 	
-	for(time=50; time<400; time*=2) //Diese for-Schleife erhöht den Wert der Zeit
-	{
-		for (lednr=1; lednr<=4; lednr++) //for_schlaufe um die Leds anzusteuern
-		{
-			led_set(lednr,1);
-			delay(time); //time Variable wird für die Verzögerung genutzt
-			led_set(lednr,0);
-			delay(time);
-		}
+	switch (key) { //ermöglicht verschiedene Fallunterscheidungen
+		case 'A': //bestimmter Fall(csae) von der char Variable hier: 'A' 
+		led_set(1,1); //Anweisung bei diesem case hier: Led 1 einschalten
+		break; //beendet jeweiligen case und switch-Anweisung
+		case 'a':
+		led_set(1,0);
+		break;
+		case 'B':
+		led_set(2,1);
+		break;
+		case 'b':
+		led_set(2,0);
+		break;
+		case 'C':
+		led_set(3,1);
+		break;
+		case 'c':
+		led_set(3,0);
+		break;
 	}
 }
+
+//char Variablen speichern immer ein Buchstabe oder ein Zeichen ab. 
