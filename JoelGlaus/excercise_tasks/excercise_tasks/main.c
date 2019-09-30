39c2f1189949c07f8c443ea6779c34caf6c09363
JoelGlaus <55971135+JoelGlaus@users.noreply.github.com>
2019-09-30 11:29:19 +0200

    Update main.c

--- JoelGlaus/excercise_tasks/excercise_tasks/main.c
+++ JoelGlaus/excercise_tasks/excercise_tasks/main.c
@@ -1,17 +1,17 @@
-/* Includes für den Zugriff auf die NIBO Burger Bibliothek */
+/* Includes fÃ¼r den Zugriff auf die NIBO Burger Bibliothek */
 #include "niboburger/robomain.h"
 
 void setup()
 {
 	/* Programmcode zum einmaligen Setup des Roboters */
-	led_init();					//Anweisung um Led zu initialisieren
+	led_init();				//Anweisung um Led zu initialisieren
 }
 
 void loop()
 {
-	/* Programmcode, den der NIBO Burger immer wieder ausführen soll */
+	/* Programmcode, den der NIBO Burger immer wieder ausfÃ¼hren soll */
 	led_set(1,1);				//Led 1 (rote LED) auf Zustand 1 setzen
-	delay(500);					//Zeitverzögerung um 500ms
+	delay(500);					//ZeitverzÃ¶gerung um 500ms
 	led_set(1,0);				//Led 1 (rote LED) auf Zustand 0 setzen
-	delay(500);					//Zeitverzögerung um 500ms
+	delay(500);					//ZeitverzÃ¶gerung um 500ms
 }
