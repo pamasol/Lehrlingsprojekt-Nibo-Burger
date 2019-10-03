f3e4ba8deb9b9ddcd3b10be57a2d6cd320739ef4
JoelGlaus <glaus.joel@gmail.com>
2019-10-03 15:38:47 +0200

    round trip, motpwm -> fährt nicht gerade

--- JoelGlaus/excercise_tasks/excercise_tasks/main.c
+++ JoelGlaus/excercise_tasks/excercise_tasks/main.c
@@ -1,60 +1,240 @@
 #include "niboburger/robomain.h"
 
-int endekalibrierunglinks;
-int endekalibrierungrechts;
-int startkalibrierung;
+/************************************************************************/
+/* STATEMACHINE                                                         */
+/************************************************************************/
 
-void kalibrierung()
+unsigned char state = 1;
+int Motorstart;
+int endeodometrielinks;
+int endeodometrierechts;
+
+void stateMachine()
 {
-	
-	motpwm_setLeft(200);
-	motpwm_setRight(200);
-	
-	if (odometry_getLeft(0)>5)
+	switch( state )
 	{
-		led_set(1,0);
-		led_set(2,1);
-		motpwm_setLeft(0);
-		endekalibrierunglinks = 1;
-	}
-	
-	if (odometry_getRight(0)>5)
-	{
-		led_set(4,0);
-		led_set(3,1);
-		motpwm_setRight(0);
-		endekalibrierungrechts = 1;
-	}
-	
-	if ((endekalibrierunglinks = 1)&&(endekalibrierungrechts = 1))
-	{
-		odometry_reset();
-		loop();
-	}
+
+		// Warten bis jemand Taster 1 drückt
+		case 1:
+			led_setall(0,0,0,0);
+			odometry_reset();
+			motpwm_setLeft(0);
+			motpwm_setRight(0);
+			endeodometrielinks = 0;
+			endeodometrierechts = 0;
+
+			if(key_get_char() == 'A') 
+			{
+				if (odometry_getLeft(0)<5)
+				{
+					led_set(1,1);
+				}
+
+				if (odometry_getRight(0)<5)
+				{
+					led_set(4,1);
+				}
+				Motorstart = 1;
+				state = 2;
+			}
+		break;
+
+		// Räder ausrichten
+		case 2:
+			if(key_get_char() == 'C') state = 1;
+
+			if (Motorstart == 1)
+			{
+				motpwm_setLeft(500);
+				motpwm_setRight(500);
+				Motorstart = 0;
+			}
+
+			if (odometry_getLeft(0)>5)
+			{
+				led_set(1,0);
+				led_set(2,1);
+				motpwm_setLeft(0);
+				endeodometrielinks = 1;
+			}
+			
+			if (odometry_getRight(0)>5)
+			{
+				led_set(4,0);
+				led_set(3,1);
+				motpwm_setRight(0);
+				endeodometrierechts = 1;
+			}
+				
+			if ((endeodometrielinks == 1)&&(endeodometrierechts == 1))
+			{
+				endeodometrielinks = 0;
+				endeodometrierechts = 0;
+				odometry_reset();
+				state = 3;
+			}
+			
+			break;
+			
+			case 3:
+			if(key_get_char() == 'C') state = 1;
+			
+			odometry_reset();
+
+			if(key_get_char() == 'B')
+			{
+				led_setall(0,0,0,0);
+				
+				if (odometry_getLeft(0)<5)
+				{
+					led_set(1,1);
+				}
+
+				if (odometry_getRight(0)<5)
+				{
+					led_set(4,1);
+				}
+				Motorstart = 1;
+				state = 4;
+				
+			}
+			break;
+
+			// Räder ausrichten
+			case 4:
+			if(key_get_char() == 'C') state = 1;
+
+			if (Motorstart == 1)
+			{
+				motpwm_setLeft(500);
+				motpwm_setRight(500);
+				Motorstart = 0;
+			}
+
+			if (odometry_getLeft(0)>500)
+			{
+				led_set(1,0);
+				led_set(2,1);
+				motpwm_setLeft(0);
+				endeodometrielinks = 1;
+			}
+			
+			if (odometry_getRight(0)>500)
+			{
+				led_set(4,0);
+				led_set(3,1);
+				motpwm_setRight(0);
+				endeodometrierechts = 1;
+			}
+			
+			if ((endeodometrielinks == 1)&&(endeodometrierechts == 1))
+			{
+				endeodometrielinks = 0;
+				endeodometrierechts = 0;
+				odometry_reset();
+				Motorstart = 1;
+				delay(500);
+				state = 5;
+			}
+			
+			break;
+			
+			case 5:
+			if(key_get_char() == 'C') state = 1;
+
+			if (Motorstart == 1)
+			{
+				motpwm_setLeft(500);
+				Motorstart = 0;
+			}
+
+			if (odometry_getLeft(0)>257)
+			{
+				led_set(1,0);
+				led_set(2,1);
+				motpwm_setLeft(0);
+				endeodometrielinks = 1;
+				odometry_reset();
+				Motorstart = 1;
+				state = 6;
+			}
+			
+			break;
+			
+			case 6:
+			if(key_get_char() == 'C') state = 1;
+			
+			odometry_reset();
+
+			if(Motorstart == 1);
+			{
+				led_setall(0,0,0,0);
+				
+				if (odometry_getLeft(0)<5)
+				{
+					led_set(1,1);
+				}
+
+				if (odometry_getRight(0)<5)
+				{
+					led_set(4,1);
+				}
+				
+				state = 7;
+				
+			}
+			break;
+
+			// Räder ausrichten
+			case 7:
+			if(key_get_char() == 'C') state = 1;
+
+			if (Motorstart == 1)
+			{
+				motpwm_setLeft(500);
+				motpwm_setRight(500);
+				Motorstart = 0;
+			}
+
+			if (odometry_getLeft(0)>500)
+			{
+				led_set(1,0);
+				led_set(2,1);
+				motpwm_setLeft(0);
+				endeodometrielinks = 1;
+			}
+			
+			if (odometry_getRight(0)>500)
+			{
+				led_set(4,0);
+				led_set(3,1);
+				motpwm_setRight(0);
+				endeodometrierechts = 1;
+			}
+			
+			if ((endeodometrielinks == 1)&&(endeodometrierechts == 1))
+			{
+				endeodometrielinks = 0;
+				endeodometrierechts = 0;
+				odometry_reset();
+
+			}
+			
+			break;
+	}	
 }
 
 
 void setup()
 {
-led_init();
-analog_init();
-motpwm_init();
-motpid_init();
-odometry_init();
+	led_init();
+	analog_init();
+	motpwm_init();
+	odometry_init();
 }
 
 void loop()
 {
-	
-char key = key_get_char();
-
-switch (key)
-	{
-	case 'A':
-	led_setall(1,0,0,1);
-	startkalibrierung = 1;
-	kalibrierung();
-	break;
-	}
+	stateMachine();
 }
 
