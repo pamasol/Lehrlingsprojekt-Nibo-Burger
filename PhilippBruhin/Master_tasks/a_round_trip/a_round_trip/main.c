4e8a499406db74f2d3039cc087bd21b9ade3e978
philippbruhin <info@philippbruhin.ch>
2019-10-04 18:04:26 +0200

    bringing wheels into startposition

--- PhilippBruhin/Master_tasks/a_round_trip/a_round_trip/main.c
+++ PhilippBruhin/Master_tasks/a_round_trip/a_round_trip/main.c
@@ -36,10 +36,10 @@ void blink_led(int led, int count) {
  *  @return enum    EVENT_NONE, EVENT_KEY1, EVENT_KEY2, EVENT_KEY3
  */
 int key_getEvent() {
-	int c = key_get_char();
-	if (c=='a') return EVENT_KEY1;
-	if (c=='b') return EVENT_KEY2;
-	if (c=='c') return EVENT_KEY3;
+	int c = key_get_state();
+	if (c==KEY_STATE_1 ) return EVENT_KEY1;
+	if (c==KEY_STATE_2 ) return EVENT_KEY2;
+	if (c==KEY_STATE_3 ) return EVENT_KEY3;
 	return EVENT_NONE;
 }
 
@@ -57,52 +57,64 @@ void stateMachine() {
             state = 2;
         break;
         
-        // Bringing wheels into start position
-        case 2:    
+        // Running wheels for alignment
+        case 2: 
+            if (key_getEvent()==EVENT_KEY3) {
+                state = 1;
+            }                
+                
             if(key_getEvent()==EVENT_KEY1 && run == 0) {
-                motpwm_setLeft(100);
-                motpwm_setRight(100);
+                motpwm_setLeft(300);
+                motpwm_setRight(300);
                 run = 1;
                 odometry_reset();
-            }
-                    
-            if (odometry_getLeft(0)>=10) {
+                state = 3;
+            }    
+
+        break;
+
+        // Stopping wheels where odometry counter is 20
+        case 3:
+            if (key_getEvent()==EVENT_KEY3) {
+                state = 1;
+            }                
+                
+            if (odometry_getLeft(0)>=20) {
                 motpwm_setLeft(0);
                 led_set(1, 0);
                 led_set(2, 1);
             }
-                    
-            if (odometry_getRight(0)>=10) {
+            
+            if (odometry_getRight(0)>=20) {
                 motpwm_setRight(0);
                 led_set(4, 0);
                 led_set(3, 1);
             }
-                    
-            state = 3;
-        break;
+            
+            if(odometry_getLeft(0)>=10 && odometry_getRight(0)>=10) {
+                run = 0;                
+            }
 
-        case 3:
-            state = 4;
         break;
 
         case 4:
-            state = 5;
+
         break;
 
         case 5:
-            state = 6;
+
         break;
 
         case 6:
-            state = 7;
+
         break;
 
         case 7:
-            state = 8;
+
         break;
 
         case 8:
-            state = 1;
+ 
         break;
     }
 }
@@ -115,7 +127,8 @@ void setup() {
 	odometry_init();
     	
 	// PID controller setpoint to zero
-	motpid_stop(1);   
+	motpid_stop(1);  
+    run = 0; 
 }
 
 void loop() {
