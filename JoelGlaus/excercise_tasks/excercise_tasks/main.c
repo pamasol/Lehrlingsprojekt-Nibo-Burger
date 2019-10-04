f6fbebbd0d7c5d56e1941e4e28c4d6c01ea68253
JoelGlaus <glaus.joel@gmail.com>
2019-10-04 09:08:19 +0200

    pfusch von philipp kommt ab jetzt

--- JoelGlaus/excercise_tasks/excercise_tasks/main.c
+++ JoelGlaus/excercise_tasks/excercise_tasks/main.c
@@ -1,74 +1,282 @@
 #include "niboburger/robomain.h"
 
-// Die zwei Leds auf jener Seite wechseln sich ab, wenn die Umdrehungen auf den jeweiligen Seiten erreicht wurden.
-// Taster 1: Fahren mit MotPWM -> Roboter fährt nicht geradeaus, da sich einer der beiden Motoren verspätet
-// Taster 2: Fahren mit MotPID -> Roboter fährt geradeaus. Eventuell kleine Abweichungen durch den Aufbau des Getriebe.
+/************************************************************************/
+/* STATEMACHINE                                                         */
+/************************************************************************/
+
+unsigned char state = 1;
+int Motorstart;
+int endeodometrielinks;
+int endeodometrierechts;
+int speedLeft;
+int speedRight;
+
+void stateMachine()
+{
+	switch( state )
+	{
+
+		// Warten bis jemand Taster 1 drückt
+		case 1:
+		led_setall(0,0,0,0);
+		odometry_reset();
+		motpwm_setLeft(0);
+		motpwm_setRight(0);
+		endeodometrielinks = 0;
+		endeodometrierechts = 0;
+
+		if(key_get_char() == 'A')
+		{
+			if (odometry_getLeft(0)<5)
+			{
+				led_set(1,1);
+			}
+
+			if (odometry_getRight(0)<5)
+			{
+				led_set(4,1);
+			}
+			Motorstart = 1;
+			state = 2;
+		}
+		break;
+
+		// Räder ausrichten
+		case 2:
+		if(key_get_char() == 'C') state = 1;
+
+		if (Motorstart == 1)
+		{
+			speedLeft = 50;
+			speedRight = 50;
+			Motorstart = 0;
+		}
+
+		if (odometry_getLeft(0)>5)
+		{
+			led_set(1,0);
+			led_set(2,1);
+			speedLeft = 0;
+			endeodometrielinks = 1;
+		}
+		
+		if (odometry_getRight(0)>5)
+		{
+			led_set(4,0);
+			led_set(3,1);
+			speedRight = 0;
+			endeodometrierechts = 1;
+		}
+		
+		if ((endeodometrielinks == 1)&&(endeodometrierechts == 1))
+		{
+			endeodometrielinks = 0;
+			endeodometrierechts = 0;
+			odometry_reset();
+			state = 3;
+		}
+		
+		motpid_setSpeed(speedLeft,speedRight);
+		
+		break;
+		
+		// Warten bis jemand taster 2 drückt
+		case 3:
+		if(key_get_char() == 'C') state = 1;
+		
+		odometry_reset();
+
+		if(key_get_char() == 'B')
+		{
+			led_setall(0,0,0,0);
+			
+			if (odometry_getLeft(0)<5)
+			{
+				led_set(1,1);
+			}
+
+			if (odometry_getRight(0)<5)
+			{
+				led_set(4,1);
+			}
+			Motorstart = 1;
+			state = 4;
+			
+		}
+		
+		motpid_setSpeed(speedLeft,speedRight);
+		
+		break;
+
+		// 1.5m vorwärts fahren
+		case 4:
+		if(key_get_char() == 'C') state = 1;
+
+		if (Motorstart == 1)
+		{
+			speedLeft = 50;
+			speedRight = 50;
+			Motorstart = 0;
+		}
+
+		if (odometry_getLeft(0)>500)
+		{
+			led_set(1,0);
+			led_set(2,1);
+			speedLeft = 0;
+			endeodometrielinks = 1;
+		}
+		
+		if (odometry_getRight(0)>500)
+		{
+			led_set(4,0);
+			led_set(3,1);
+			speedRight = 0;
+			endeodometrierechts = 1;
+		}
+		
+		if ((endeodometrielinks == 1)&&(endeodometrierechts == 1))
+		{
+			endeodometrielinks = 0;
+			endeodometrierechts = 0;
+			odometry_reset();
+			Motorstart = 1;
+			state = 5;
+		}
+		
+		motpid_setSpeed(speedLeft,speedRight);
+		
+		break;
+		
+		case 5:
+		if(key_get_char() == 'C') state = 1;
+
+		if (Motorstart == 1)
+		{
+			delay(500);
+			speedLeft = 50;
+			Motorstart = 0;
+		}
+
+		if (odometry_getLeft(0)>260)
+		{
+			led_set(1,0);
+			led_set(2,1);
+			speedLeft = 0;
+			endeodometrielinks = 1;
+			odometry_reset();
+			state = 6;
+			Motorstart = 1;
+		}
+		
+		motpid_setSpeed(speedLeft,speedRight);
+		
+		break;
+		
+		case 6:
+		if(key_get_char() == 'C') state = 1;
+		
+		odometry_reset();
+
+		if(Motorstart == 1);
+		{
+			led_setall(0,0,0,0);
+			
+			if (odometry_getLeft(0)<5)
+			{
+				led_set(1,1);
+				odometry_reset();
+			}
+
+			if (odometry_getRight(0)<5)
+			{
+				led_set(4,1);
+				odometry_reset();
+			}
+			
+			state = 7;
+			
+		}
+		
+		motpid_setSpeed(speedLeft,speedRight);
+		
+		break;
+
+		// Räder ausrichten
+		case 7:
+		if(key_get_char() == 'C') state = 1;
+
+		if (Motorstart == 1)
+		{
+			speedLeft = 50;
+			speedRight = 50;
+			Motorstart = 0;
+		}
+
+		if (odometry_getLeft(0)>500)
+		{
+			led_set(1,0);
+			led_set(2,1);
+			speedLeft = 0;
+			endeodometrielinks = 1;
+		}
+		
+		if (odometry_getRight(0)>500)
+		{
+			led_set(4,0);
+			led_set(3,1);
+			speedRight = 0;
+			endeodometrierechts = 1;
+		}
+		
+		if ((endeodometrielinks == 1)&&(endeodometrierechts == 1))
+		{
+			endeodometrielinks = 0;
+			endeodometrierechts = 0;
+			Motorstart = 1;
+			odometry_reset();
+			state = 8;
+
+		}
+		
+		motpid_setSpeed(speedLeft,speedRight);
+		
+		break;
+		
+		case 8:
+		if(key_get_char() == 'C') state = 1;
+
+		if (Motorstart == 1)
+		{
+			speedLeft = 50;
+			Motorstart = 0;
+		}
+
+		if (odometry_getLeft(0)>260)
+		{
+			led_set(4,0);
+			led_set(3,1);
+			speedLeft = 0;
+			odometry_reset();
+		}
+		
+		motpid_setSpeed(speedLeft,speedRight);
+		
+		break;
+	}
+}
+
 
 void setup()
 {
 	led_init();
-	analog_init();		
+	analog_init();
 	motpwm_init();
-	motpid_init();
 	odometry_init();
+	motpid_init();
 }
 
 void loop()
 {
-	char key = key_get_char();
-	switch (key) 
-	
-	{
-		//MotPWM
-		case 'A':
-		motpwm_setLeft(200);
-		motpwm_setRight(200);
-		break;
-		
-		//MotPID
-		case 'B':
-		motpid_setSpeed(9,9);
-		break;
-		
-		//RESET
-		case 'C':
-		motpwm_setLeft(0);
-		motpwm_setRight(0);
-		motpid_setSpeed(0,0);
-		break;
-	}
-	
-	if (odometry_getLeft(0)<500)
-	{
-		led_set(1,1);
-		led_set(2,0);
-	}
-	
-	if (odometry_getLeft(0)>500)
-	{
-		led_set(1,0);
-		led_set(2,1);
-	}
-	
-	if (odometry_getLeft(0)>1000)
-	{
-		odometry_reset();
-	}
-	
-	if (odometry_getRight(0)<500)
-	{
-		led_set(4,1);
-		led_set(3,0);
-	}
-	
-	if (odometry_getRight(0)>500)
-	{
-		led_set(4,0);
-		led_set(3,1);
-	}
-	
-	if (odometry_getRight(0)>1000)
-	{
-		odometry_reset();
-	}
+	stateMachine();
 }
