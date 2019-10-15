ba9a26603b413afed250ec4d8770f87f273381d3
marvinbueeler <marvin_00@outlook.com>
2019-10-15 11:39:18 +0200

    Fraidy Cat

--- MarvinBueeler/excercise_tasks/excercise_tasks/main.c
+++ MarvinBueeler/excercise_tasks/excercise_tasks/main.c
@@ -7,113 +7,282 @@
 
 #include "niboburger/robomain.h"
 
-int odometrie_kali;
-int runde;
-int zaehler;
-int starterlaubnis;
-int drehen;
+unsigned char state;
+int abstand = 30;
 
-void odometrie_kalibrieren()
-{
-	odometry_reset();
-	do
-	{
-		if (odometry_getLeft(0)<1)
-		{
-			led_setall(1,0,0,1);
-			motpid_setSpeed(10,10);
-		} else if (odometry_getLeft(0)>1)
-		{
-			led_setall(0,1,1,0);
-			motpid_stop(1);
-			odometrie_kali = 0;
-			starterlaubnis = 1;
-			odometry_reset();
-			loop();
-		}
-	} while (odometrie_kali == 1);
-}
+/**********************************
 
-void rundedrehen()
+Statemachine
+
+**********************************/
+
+void statemachine()
 {
-	do
+	
+	/**********************************
+	
+	Sensorwerte für Objekterkennung initialisieren
+	
+	**********************************/
+	
+	int fll = analog_getValueExt(ANALOG_FLL, 2);
+	int fl = analog_getValueExt(ANALOG_FL, 2);
+	int frr = analog_getValueExt(ANALOG_FRR, 2);
+	int fr = analog_getValueExt(ANALOG_FR, 2);
+	
+	/**********************************
+	
+	LED's über Sensoren steuern
+	
+	**********************************/
+	
+	if (fll > abstand)
 	{
-		if (odometry_getLeft(0)<1083)
-		{
-			motpid_setSpeed(50,50);
-		}
-		if (odometry_getLeft(0)>1083)
-		{
-			odometry_reset();
-			drehen = 1;
-			do 
+		led_set(1,1);
+	} else
+	{
+		led_set(1,0);
+	}
+	
+	if (fl > abstand)
+	{
+		led_set(2,1);
+	} else
+	{
+		led_set(2,0);
+	}
+	
+	if (fr > abstand)
+	{
+		led_set(3,1);
+	} else
+	{
+		led_set(3,0);
+	}
+	
+	if (frr > abstand)
+	{
+		led_set(4,1);
+	} else
+	{
+		led_set(4,0);
+	}
+	
+	
+	switch(state)
+	{
+		case 1:
+		
+			/**********************************
+			
+			Anzeigen dass er im State 1 Modus ist
+			
+			**********************************/
+			
+			motpid_setSpeed(0,0);
+			led_setall(1,0,0,0);
+			delay(200);
+			led_setall(0,1,0,0);
+			delay(200);
+			led_setall(0,0,1,0);
+			delay(200);
+			led_setall(0,0,0,1);
+			delay(200);
+			
+			/**********************************
+			
+			Auf Taster 1 warten, damit Start signalisiert werden kann
+			
+			**********************************/
+			
+			if (key_get_char() == 'A')
 			{
-				if (odometry_getLeft(0)<130)
+				
+				led_setall(1,0,1,0);
+				delay(500);
+				led_setall(0,1,0,1);
+				delay(500);
+				led_setall(0,0,0,0);
+				state = 2;
+				
+			}
+			
+		break;
+		
+		case 2:
+			
+			/**********************************
+			
+			Vorwaerts fahren bis ein Sensor anspricht
+			
+			**********************************/
+			if ((fll < abstand) && (fl < abstand) && (fr < abstand) && (frr < abstand))
+			{
+				
+				motpid_setSpeed(50,50);
+				
+			}
+			
+			if ((fl) > abstand)
+			{
+				
+				state = 3;
+				
+			}
+		
+			if ((fll) > abstand)
+			{
+				
+				state = 3;
+				
+			}
+			
+			if ((frr) > abstand)
+			{
+				
+				state = 4;
+				
+			}
+			
+			if ((fr) > abstand)
+			{
+				
+				state = 4;
+				
+			}
+			
+			/**********************************
+			
+			Taster C ist der Ausschalttaster
+			
+			**********************************/
+			
+			if (key_get_char() == 'C')
+			{
+				
+				motpid_setSpeed(0,0);
+				led_setall(1,0,0,1);
+				delay(1000);
+				led_setall(0,0,0,0);
+				state = 1;
+				
+			}
+					
+		break;
+		
+		/**********************************
+		
+		Rechtsmanoever
+		
+		**********************************/
+		
+		case 3:
+			
+			motpid_setSpeed(50, -50);
+			
+			if((fll < abstand) && (fl < abstand))
+			{
+				
+				if ((frr) > abstand)
 				{
-					motpid_setSpeed(50,-50);
+					
+					state = 5;
+					
 				}
-				if (odometry_getLeft(0)>130)
+				
+				state = 2;
+				
+			}
+			
+		break;
+			
+		/**********************************
+		
+		Linksmanoever 
+		
+		**********************************/
+		
+		case 4:
+			
+			motpid_setSpeed(-50, 50);
+			
+			if ((frr < abstand) && (fr < abstand))
+			{
+				
+				if ((fll) > abstand)
 				{
-					motpid_setSpeed(0,0);
-					drehen = 0;
+					
+					state = 6;
+					
 				}
-			} while (drehen ==1);
-			zaehler = zaehler + 1;
-			odometry_reset();
-		}
-		if (zaehler==2)
-		{
-			motpid_stop(1);
-			runde = 0;
-			starterlaubnis = 0;
-			zaehler = 0;
-			led_setall(1,0,0,1);
-			odometry_reset();
-			loop();
-		}
-	} while (runde == 1);
+				
+				state = 2;
+				
+			}
+			
+		break;
+		
+		/**********************************
+		
+		Rechtsmanoever bei Erkennung von Wand rechts
+		bei Rechtsmanoever
+		
+		**********************************/
+		
+		case 5:
+			
+			motpid_setSpeed(50, -50);
+			
+			if ((frr < abstand) && (fr < abstand))
+			{
+				
+				state = 3;
+				
+			}
+			
+		break;
+		
+		/**********************************
+		
+		Linksmanoever bei Erkennung von Wand links
+		bei linksmanoever
+		
+		**********************************/
+		
+		case 6:
+		
+			motpid_setSpeed(-50, 50);
+			
+			if ((fll < abstand) && (fl < abstand))
+			{
+				
+				state = 4;
+				
+			}
+			
+		break;
+			
+	}
 }
 
-void setup() //Setup wird einmal am Anfang ausgeführt
+/**********************************
+
+Setup wird beim Start durchgeführt, hier werden Sensoren usw. initialisiert
+
+**********************************/
+
+void setup() 
 {
-	led_init(); //Leds werden initialisiert
-	analog_init(); //alle analogen Datenleitungen (auch die für die Taster) werden initialisiert 
-	odometry_init(); //Odometry Sensoren werden initialisiert
-	motpid_init(); //Motoren werden initialisiert
-	surface_readPersistent(); //Laden der Kalibrierung aus EEPROM
+	led_init(); 
+	analog_init(); 
+	odometry_init(); 
+	motpid_init(); 
+	surface_readPersistent(); 
 	odometry_reset();
-	led_setall(1,0,0,1);
-	odometrie_kali = 0;
-	runde = 0;
-	zaehler = 0;
-	starterlaubnis = 0;
-	drehen = 0;
+	state = 1;
 }
 
 void loop() 
 {
-	char key = key_get_char();
-	
-	switch (key)
-	{
-		/*
-		Odometrie kalibrieren
-		*/
-		case 'a':
-			if(starterlaubnis == 0)
-				{
-					odometrie_kali = 1;
-					odometrie_kalibrieren();
-				}
-		break;
-		
-		case 'b':
-			if(starterlaubnis == 1)
-				{
-					runde = 1;
-					rundedrehen();
-				}
-		break;
-			
-	}
+	statemachine();
 }
