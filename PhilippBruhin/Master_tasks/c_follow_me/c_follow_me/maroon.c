b2add565c11b7b2778083d553d84cdf861051cda
philippbruhin <info@philippbruhin.ch>
2019-10-17 19:52:53 +0200

    some comments in maroon shield

--- PhilippBruhin/Master_tasks/c_follow_me/c_follow_me/maroon.c
+++ PhilippBruhin/Master_tasks/c_follow_me/c_follow_me/maroon.c
@@ -38,6 +38,13 @@ void maroon_bars(uint8_t a, uint8_t b, uint8_t c, uint8_t d, uint8_t e, uint8_t 
 }
 
 void maroon_setup() {
+    
+/** For universal asynchronous communication following parameters have to
+ *  be in common
+ *  1. Transmission speed in baud per second
+ *	2. Data length in bits
+ *  3. Start/Stop bit
+ */
     usart_setbaudrate(38400);
     usart_enable();
 }
