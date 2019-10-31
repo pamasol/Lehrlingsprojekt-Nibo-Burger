25f48593b3f9632b925e8405026caf56179dc6c9
JonasBisig <jonas.bisig@gmx.ch>
2019-10-31 11:16:52 +0100

    Den Wert zum geradeaus fahren auf 20 geaendert

--- JonasBisig/NIBO Burger_Aufgabe 16/NIBO Burger_Aufgabe 16/main.c
+++ JonasBisig/NIBO Burger_Aufgabe 16/NIBO Burger_Aufgabe 16/main.c
@@ -6,16 +6,16 @@ void setup()
 	led_init();
 	analog_init();
 	motpwm_init();
-}
-
-void test() {
-	// das ist eine testfunktion
+	
+	
 }
 
 void loop()
 {
+	int key;
+	int status;
 	analog_wait_update();
-	
+	if
 	int fll = analog_getValueExt(ANALOG_FLL, 2);
 	int fl = analog_getValueExt(ANALOG_FL, 2);
 	int fr = analog_getValueExt(ANALOG_FR, 2);
@@ -24,18 +24,26 @@ void loop()
 	int l = max(fll , fl);
 	int r = max(frr , fr);
 	
-	if ((l<10) && (r<10))
+	switch (key)
+	{
+		case 'a':
+		status = 1;
+	}
+	if ((l<20) && (r<20))
 	{
 		motpwm_setLeft(800);
 		motpwm_setRight(800);
+		led_setall(1,0,0,1);
 	} else if (l<r)
 	{
 		motpwm_setLeft(0);
 		motpwm_setRight(800);
+		led_setall(0,0,1,0);
 	} else if (l>r)
 	{
 		motpwm_setLeft(800);
 		motpwm_setRight(0);
+		led_setall(0,1,0,0);
 	}
 	
 }
