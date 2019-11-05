fd7ede3bbb236cc4f20789e29bf62271cd06dd61
marvinbueeler <marvin_00@outlook.com>
2019-11-05 16:25:40 +0100

    Rabbit warren

--- MarvinBueeler/excercise_tasks/excercise_tasks/main.c
+++ MarvinBueeler/excercise_tasks/excercise_tasks/main.c
@@ -9,9 +9,17 @@
 
 int state;
 int abstand = 20;
+int m;
+int r;
+int l;
+int key;
+int speed1 = 30;
+int speed2 = 20;
+int speed3 = -20;
 
 void setup() 
 {
+
 	led_init(); 
 	analog_init(); 
 	odometry_init(); 
@@ -23,54 +31,88 @@ void setup()
 
 void loop() 
 {	
-	analog_wait_update();
-	nibo_checkMonitorVoltage(); 
+	key = key_get_char();
+	m = surface_get(SURFACE_C);
+	r = surface_get(SURFACE_R);
+	l = surface_get(SURFACE_L);
 	
-	unsigned long int col = surface_getColorRGB();
 	
-	int diff_red = color_diff_rgb(col, COLOR_RGB_CAL_RED);
-	int diff_black = color_diff_rgb(col, COLOR_RGB_CAL_BLACK);
-	int diff_blue = color_diff_rgb(col, COLOR_RGB_CAL_BLUE);
-	int diff_green = color_diff_rgb(col, COLOR_RGB_CAL_GREEN);
-	int diff_yellow = color_diff_rgb(col, COLOR_RGB_CAL_YELLOW);
+	if (m < 20)
+	{
+		led_set(1,1);
+		led_set(4,1);
+	} else
+	{
+		led_set(1,0);
+		led_set(4,0);
+	}
+	if (l < 200)
+	{
+		led_set(2,1);
+	} else
+	{
+		led_set(2,0);
+	}
+	if (r < 200)
+	{
+		led_set(3,1);
+	} else
+	{
+		led_set(3,0);
+	}
 	
-// 	led_set(1, diff_red < 8500);
-// 	led_set(2, diff_yellow < 3500);
- 	led_set(3, diff_blue < 4500);
- 	led_set(4, diff_green < 3000);
-	 
-	 if (diff_red < 9000)
-	 {
-		 if (diff_yellow < 4000)
-		 {
-			 led_set(2,1);
-			 led_set(1,0);
-		 } 
-		 else
-		 {
-			 led_set(1,1);
-			 led_set(2,0);
-		 }
-	 } else
-	 {
-		 led_set(2,0);
-		 led_set(1,0);
-	 }
+	switch (state)
+	{
+		case 0:
+		
+			motpid_setSpeed(0,0);
+			
+			if (key == 'A')
+			{
+				state = 1;
+			}
+			
+		break;
+		
+		case 1:
+		
+			motpid_setSpeed(speed1,speed1);
+			
+			if (m > 20)
+			{
+				state = 2;
+			}
+			
+			if (key == 'B')
+			{
+				state = 0;
+			}
+			
+		break;
+		
+		case 2:
+			
+			if (l < 300)
+			{
+				motpid_setSpeed(speed3,speed2);
+			}
+			if (r < 200)
+			{
+				motpid_setSpeed(speed2,speed3);
+			}
+			if (m < 20)
+			{
+				motpid_setSpeed(0,0);
+				state = 1;
+			}
+			
+			if (key == 'B')
+			{
+				state = 0;
+			}
+			
+		break;
+	}
 
-// 	if (diff_green < 2500)
-// 	{
-// 		led_set(4,1);
-// 	} else
-// 	{
-// 		led_set(4,0);
-// 	}
-// 	
-// 	if (diff_blue < 4000)
-// 	{
-// 		led_set(3,1);
-// 	} 
-// 	else
-// 	{
-// 		led_set((3,0);
-// 	}
+
 }
