da28e40c860080bf981427f01772b7d19f6fbc3e
JoelGlaus <glaus.joel@gmail.com>
2019-12-10 14:35:50 +0100

    Follow Me

--- JoelGlaus/excercise_tasks/excercise_tasks/main.c
+++ JoelGlaus/excercise_tasks/excercise_tasks/main.c
@@ -4,30 +4,11 @@
 /* STATEMACHINE                                                         */
 /************************************************************************/
 
-unsigned char state = 0;
-int Start = 0;
-int StatusLED = 1;
-int FL = 0;
-int FR = 0;
+
 
 
 void stateMachine()
 {
-	if (StatusLED == 1)
-	{
-		led_setall(1,0,0,1);
-		delay(100);
-		led_setall(0,0,0,0);
-		delay(100);
-	}
-	
-	if(key_get_char() == 'A')
-	{
-		StatusLED = 0;
-		led_setall(0,1,1,0);
-		delay(1000);
-		Start = 1;
-	}
 	
 	int fll = analog_getValueExt(ANALOG_FLL, 2);
 	int fl = analog_getValueExt(ANALOG_FL, 2);
@@ -35,127 +16,60 @@ void stateMachine()
 	int fr = analog_getValueExt(ANALOG_FR, 2);
 	int frr = analog_getValueExt(ANALOG_FRR, 2);
 	
-	if ((fll>25)&&(Start == 1))
+	if (fll>22)
 	{
 		led_set(1, 1);
-		state = 1;
+		motpid_setSpeed(0,50);
 	}
 	else
 	{
 		led_set(1, 0);
 	}
 
-	if ((fl>25)&&(Start == 1))
+	if (fl>22)
 	{
 		led_set(2, 1);
-		FL = 1;
-		state = 1;
+		motpid_setSpeed(0,50);
 	}
 	else
 	{
 		led_set(2, 0);
 	}
 
-	if ((fr>25)&&(Start == 1))
+	if (fr>22)
 	{
 		led_set(3, 1);
-		FR = 1;
-		state = 2;
+		motpid_setSpeed(50,0);
 	}
 	else
 	{
 		led_set(3, 0);
 	}
 
-	if ((frr>25)&&(Start == 1))
+	if (frr>22)
 	{
 		led_set(4, 1);
-		state = 2;
+		motpid_setSpeed(50,0);
 	}
 	else
 	{
 		led_set(4, 0);
 	}
 	
-	if((fr>25)&&(fl>25)&&(Start == 1))
+	if ((fr>22)&&(fl>22))
 	{
-	motpid_setSpeed(-50,50);
-	delay (500);
-	motpid_setSpeed(0,0);
+		motpid_setSpeed(50,50);
 	}
 	
-	switch( state )
+	if ((fll<22)&&(fl<22)&&(fr<22)&&(frr<22))
 	{
-
-		case 0:
-		if (Start == 1)
-		{
-			motpid_setSpeed(50,50);
-		}
-		
-		break;
-		
-		case 1:
-		if ((fr>25)&&(frr>25))
-		{
-			motpid_setSpeed(50,-50);
-			delay(1000);
-			motpid_setSpeed(0,0);
-			state = 0;
-		}
-		
-		if ((fr<25)&&(frr<25))
-		{
-			motpid_setSpeed(50,-50);
-			delay(500);
-			motpid_setSpeed(0,0);
-			state = 0;
-		}
-		
-		if ((FR == 1)&&(FL == 1))
-		{
-			motpid_setSpeed(50,-50);
-			delay (500);
-			motpid_setSpeed(0,0);
-			FR = 0;
-			FL = 0;
-			state = 0;
-		}
-		
-		
-		break;
-		
-		case 2:
-		
-		if ((fl>25)&&(fll>25))
-		{
-			motpid_setSpeed(-50,50);
-			delay(1000);
-			motpid_setSpeed(0,0);
-			state = 0;
-		}
-		
-		if ((fl<25)&&(fll<25))
-		{
-			motpid_setSpeed(-50,50);
-			delay(500);
-			motpid_setSpeed(0,0);
-			state = 0;
-		}
-		
-		if ((FR == 1)&&(FL == 1))
-		{
-			motpid_setSpeed(50,-50);
-			delay (500);
-			motpid_setSpeed(0,0);
-			FR = 0;
-			FL = 0;
-			state = 0;
-		}
-		
-		break;
-		
+		motpid_setSpeed(0,0);
+		led_setall(1,0,0,1);
+		delay(100);
+		led_setall(0,0,0,0);
+		delay(100);
 	}
+	
 }
 
 
@@ -173,3 +87,6 @@ void loop()
 	stateMachine();
 	
 }
+		
+	
+	
