38771e95d897a360d592527ac3c770b8369894ea
david-bernhard <david.bernhard@pamasol.com>
2019-12-13 16:32:16 +0100

    Aufgabe ff gelöst

--- DavidBernhard/Exercise Tasks/NIBO Burger_Vorlage1/NIBO Burger_Vorlage1/main.c
+++ DavidBernhard/Exercise Tasks/NIBO Burger_Vorlage1/NIBO Burger_Vorlage1/main.c
@@ -1,27 +1,30 @@
-/* Includes für den Zugriff auf die NIBO Burger Bibliothek */
-#include "niboburger/robomain.h"
-int LED;
-void setup()
-{
-	led_init();
-}
+   
 
-void loop()
-{
-led_set(1, 1);
-delay(500);
-led_set(1, 0);
-delay(500);
-led_set(2, 1);
-delay(500);
-led_set(2, 0);
-delay(500);
-led_set(3, 1);
-delay(500);
-led_set(3, 0);
-delay(500);
-led_set(4, 1);
-delay(500);
-led_set(4, 0);
-delay(500);
-}
+   /* Includes für den Zugriff auf die NIBO Burger Bibliothek */
+
+   #include "niboburger/robomain.h"
+
+   int LED;
+   int Time;
+   void setup()
+   {
+	   led_init();
+   }
+
+
+
+   void loop()
+   {
+	  
+	   for (LED=1; LED<=4;LED++)
+	   {
+	   for (Time=50; Time<=200; Time*2)
+	   {
+	   
+		   led_set(LED, 1);
+		   delay(Time);
+		   led_set(LED, 0);
+		   delay(Time);
+	   }
+	   }
+   }
