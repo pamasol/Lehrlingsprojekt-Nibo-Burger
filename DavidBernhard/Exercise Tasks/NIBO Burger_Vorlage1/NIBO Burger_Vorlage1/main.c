0c8c705de7cb5b56db74c2238dab7b6fcbe31a56
david-bernhard <david.bernhard@pamasol.com>
2019-12-13 16:21:35 +0100

    Aufgabe d gelöst

--- DavidBernhard/Exercise Tasks/NIBO Burger_Vorlage1/NIBO Burger_Vorlage1/main.c
+++ DavidBernhard/Exercise Tasks/NIBO Burger_Vorlage1/NIBO Burger_Vorlage1/main.c
@@ -8,11 +8,20 @@ void setup()
 
 void loop()
 {
-	for (LED=1; LED<=4;LED++)
-	{
-		led_set(LED, 1);
-		delay(500);
-		led_set(LED, 0);
-		delay(500);
-	}
+led_set(1, 1);
+delay(500);
+led_set(1, 0);
+delay(500);
+led_set(2, 1);
+delay(500);
+led_set(2, 0);
+delay(500);
+led_set(3, 1);
+delay(500);
+led_set(3, 0);
+delay(500);
+led_set(4, 1);
+delay(500);
+led_set(4, 0);
+delay(500);
 }
