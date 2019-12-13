2c21d3d6ffaef2ba0e3282278cd2d78327560689
david-bernhard <david.bernhard@pamasol.com>
2019-12-13 14:53:28 +0100

    Aufgabe B gelöst

--- DavidBernhard/Exercise Tasks/a_flashing_led_1/a_flashing_led_1/main.c
+++ DavidBernhard/Exercise Tasks/a_flashing_led_1/a_flashing_led_1/main.c
@@ -1,21 +1,15 @@
-
 /* Includes for access to NIBO Burger library */
-
 #include "niboburger/robomain.h"
 
-
-
 void setup()
-
 {
+	/* Code that is executed once at startup */
 	led_init();
 }
 
-
-
 void loop()
-
 {
+	/* Code that is repeated infinitely on NIBO Burger */
 	led_set(2, 1);
 	delay(500);
 	led_set(2, 0);
