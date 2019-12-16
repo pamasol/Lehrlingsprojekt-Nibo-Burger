8c417990707fb79c375d0fbebdce35dfd2259dbd
david-bernhard <david.bernhard@pamasol.com>
2019-12-16 08:22:12 +0100

    Aufgabe g gelöst

--- DavidBernhard/Exercise Tasks/123/123/main.c
+++ DavidBernhard/Exercise Tasks/123/123/main.c
@@ -11,25 +11,23 @@
 
 
    int LED;
-   int Time;
 
    void setup()
    {
 	   led_init();
+	analog_init();
    }
 
    void loop()
    {
-	   for (Time=50; Time<=200; Time=Tim*2)
-	   {
-		   
-		   
-		   for (LED=1; LED<=4;LED++)
-		   {
-			   led_set(LED, 1);
-			   delay(Time);
-			   led_set(LED, 0);
-			   delay(Time);
-		   }
-	   }
+char key=key_get_char();
+switch (key)
+   {
+case 'A':
+led_set(3,1);
+break;
+case 'a':
+led_set(3,0);
+break;
+}
    }
