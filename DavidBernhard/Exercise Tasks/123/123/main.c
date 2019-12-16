b2e2822fd29ce309c321dc7af867b6d32157b4c5
david-bernhard <david.bernhard@pamasol.com>
2019-12-16 10:24:51 +0100

    Aufgabe m gelöst

--- DavidBernhard/Exercise Tasks/123/123/main.c
+++ DavidBernhard/Exercise Tasks/123/123/main.c
@@ -1,36 +1,75 @@
    
 
-
-
-   /* Includes für den Zugriff auf die NIBO Burger Bibliothek */
-
-
-
-   #include "niboburger/robomain.h"
+#include <niboburger/robomain.h>
 
 
 
-   int LED;
+void setup() {
 
-   void setup()
-   {
 	led_init();
+
 	analog_init();
-   }
 
-   void loop()
-   {
-int value = analog_getValueExt(ANALOG_FR, 2);
+}
 
-	if (value<10) {
-		led_setall(0,0,0,0);
-		} else if (value<20) {
-		led_setall(1,0,0,0);
-		} else if (value<30) {
-		led_setall(1,1,0,0);
-		} else if (value<40) {
-		led_setall(1,1,1,0);
-		} else {
-		led_setall(1,1,1,1);
-   }
-   }
+
+
+void loop() {
+
+	/*
+
+	Variables that store analog values of all 4 IR sensors in front of 
+
+	NIBO Burger.
+
+	fll	= Front left of left
+
+	fl	= Front left
+
+	fr	= Front right
+
+	frr	= Front right of right
+
+	*/
+
+	int fll = analog_getValueExt(ANALOG_FLL, 2);
+	int fl = analog_getValueExt(ANALOG_FL, 2);
+	int fr = analog_getValueExt(ANALOG_FR, 2);
+	int frr = analog_getValueExt(ANALOG_FRR, 2);
+
+if (fll<20)
+{
+led_set(1,0);
+} 
+else
+{
+led_set(1,1);
+}
+
+if (fl<20)
+{
+led_set(2,0);
+} 
+else
+{
+led_set(2,1);
+}
+
+if (fr<20)
+{
+	led_set(3,0);
+}
+else
+{
+	led_set(3,1);
+}
+
+if (frr<20)
+{
+	led_set(4,0);
+}
+else
+{
+	led_set(4,1);
+}
+}
