45363af16fe554fbc17d9bd02299303506fe31ff
david-bernhard <david.bernhard@pamasol.com>
2019-12-19 13:05:20 +0100

    Aufgabe EE gelöst

--- DavidBernhard/Exercise Tasks/main.c
+++ DavidBernhard/Exercise Tasks/main.c
@@ -1,151 +1,50 @@
 #include <niboburger/robomain.h>
 
+
 int state;
 
-int V1;
+int Mitte;
 
-int V2;
+int Rechts;
 
+int Links;
 
-
-
-
-
-	void setup() {
+int key;
 
 
 
-		led_init();
+void setup() {
 
-		motpid_init();
+	led_init();
 
-		motpwm_init();
+	motpid_init();
 
-		analog_init();
+	motpwm_init();
 
-		odometry_init();
+	analog_init();
 
-		odometry_reset();
+	odometry_init();
 
-		state = 1;
-		
-
-	}
-
-
-
-	void loop() {
-
-
-	int fll = analog_getValueExt(ANALOG_FLL, 2);
-
-	int fl = analog_getValueExt(ANALOG_FL, 2);
-
-	int frr = analog_getValueExt(ANALOG_FRR, 2);
-
-	int fr = analog_getValueExt(ANALOG_FR, 2);
-
-
-	int Rechts = max(frr, fr);
+	odometry_reset();
 	
-	int Links = max(fll, fl);
+	surface_readPersistent();
+
+	state = 1;
 	
+}
+
+
+
+
+
+
+
+void loop() {
+
+	led_setall(1, 1, 1, 1);
 	
-
-	switch (state)
-	
-	{
-		case 1:
-
-		motpid_setSpeed(10,10);
-
-		if (key_get_char() == 'A')
-
-		{
-
-			delay(100);
-
-			state = 2;
-
-		}
-
-		break;
+}
 
 
 
-		case 2:
 
-		if (Rechts > 40)
-
-		{
-
-			V1 = 50;
-
-		}
-
-		else if (Rechts > 30)
-
-		{
-
-			V1 = 40;
-
-		}
-
-		else if (Rechts > 20)
-
-		{
-
-			V1 = 30;
-
-		}
-
-		else
-		
-		{
-			V1 = 0;
-		}
-		
-		
-		if (Links > 40)
-
-		{
-
-			V2 = 50;
-
-		}
-
-		else if (Links > 30)
-
-		{
-
-			V2 = 40;
-
-		}
-
-		else if (Links > 20)
-
-		{
-
-			V2 = 30;
-
-		}
-
-		else
-
-		{
-			V2 = 0;
-		}
-		
-		
-
-		motpid_setSpeed(V1, V2);
-		
-		
-
-
-		break;
-		
-		
-	}
-
-	}
