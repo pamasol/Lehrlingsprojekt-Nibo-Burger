5c64d2197bff4cfaadfdb6bb1a98d931cbd55557
N00XD <nicolas.diethelm@gmx.ch>
2022-03-03 12:13:08 +0100

    added initial project for flashing LED task

---
+++ NicolasDiethelm/Excercise_tasks/a_Flashing_LED1/a_flashing_led1/main.c
@@ -0,0 +1,18 @@
+/*
+ * a_flashing_led1.c
+ *
+ * Created: 03.03.2022 12:10:14
+ * Author : nicol
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
