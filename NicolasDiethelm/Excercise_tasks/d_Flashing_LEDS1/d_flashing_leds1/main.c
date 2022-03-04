3cb96130d9cee690dd963ae3dacd5d4d59c90224
N00XD <nicolas.diethelm@gmx.ch>
2022-03-04 08:53:13 +0100

    excercise d finished

---
+++ NicolasDiethelm/Excercise_tasks/d_Flashing_LEDS1/d_flashing_leds1/main.c
@@ -0,0 +1,27 @@
+
+#include <niboburger/robomain.h>
+
+void setup() {
+	led_init();
+}
+
+void loop() {
+	delay(500);
+	led_set(1,1);
+	delay(500);
+	led_set(1,0);
+	delay(500);
+	led_set(2,1);
+	delay(500);
+	led_set(2,0);
+	delay(500);
+	led_set(3,1);
+	delay(500);
+	led_set(3,0);
+	delay(500);
+	led_set(4,1);
+	delay(500);
+	led_set(4,0);
+	
+}
+
